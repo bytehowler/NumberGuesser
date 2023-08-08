71d8b88a41e4ab82178c155bcccc36757a5ed5e6
Luke Idenburg <28677981+AlfaLynx@users.noreply.github.com>
2023-08-08 14:27:04 +0200

    Created some function definitions

--- main.cpp
+++ main.cpp
@@ -1,5 +1,27 @@
+#include <iostream>
+#include <random>
+
+void displayWelcomeMessage();
+void displayIncorrectGuessMessage();
+void displayCorrectGuessMessage();
+void displayGoodbyeMessage();
+
+void displayGuessCount(int);
+bool displayReplayPrompt();
+
+int	generateRandomNumber(int, int);
+int getPlayerGuess();
+
+
 int main()
 {
+	while (true)
+	{
+		displayWelcomeMessage();
+
+		int randomNumber = generateRandomNumber(1, 10);
+
+	}
 
 	return 0;
 }
