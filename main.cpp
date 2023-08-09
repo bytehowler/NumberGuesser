5d9c97e89e674461696a3143fd2ab954ff39646e
Luke Idenburg <28677981+AlfaLynx@users.noreply.github.com>
2023-08-09 15:52:53 +0200

    Added some comments as well as changing one of the error messages.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "GameFunctions.h"
 
+// These variables determine the minimum and maximum values for the random number generator.
 const int MIN_VALUE = 1;
 const int MAX_VALUE = 10;
 
@@ -12,6 +13,8 @@ int main()
 		int randomNumber = generateRandomNumber(MIN_VALUE, MAX_VALUE);
 		int guessCounter = 0;
 
+		// This is the main guessing loop, it keeps repeating
+		// and incrementing the guess counter untill the correct number is guessed
 		while (displayGuessPrompt() != randomNumber)
 		{
 			guessCounter++;
@@ -23,7 +26,11 @@ int main()
 		displayGuessCountMessage(guessCounter);
 
 		
-
+		// This in the replay prompt loop, it only accepts 'Y' or 'N' (case insensitive).
+		// Any other input causes the prompt to repeat until the proper input is given.
+		// 
+		// If 'N' is given, the program shuts down, 
+		// if 'Y' is given, the program starts from the top. 
 		if (!displayReplayPrompt()) {
 			displayGoodbyeMessage();
 			break;
